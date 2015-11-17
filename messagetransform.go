dd1b7eb1559f0a056ff762e41fb8b35dc48ae893
Dana M. Diederich <diederich@gmail.com>
2015-11-16 19:01:05 -0800

    basic bones

---
+++ messagetransform.go
@@ -0,0 +1,10 @@
+package messagetransform
+
+import (
+	"fmt"
+)
+
+func Transform(message *map[string]interface{}, transform map[string]interface{}) error {
+	fmt.Println("Rock and roll")
+	return nil
+}
---
+++ messagetransform_basic_test.go
@@ -0,0 +1,23 @@
+package messagetransform
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//func Transform(message *map[string]interface{}, transform map[string]interface{}) error {
+//basic tests from reference implementation:
+//https://github.com/dana/perl-Message-Transform/blob/master/t/basic.t
+func TestSimplestTransform(t *testing.T) {
+	assert := assert.New(t)
+	message := map[string]interface{}{
+		"a": "b",
+	}
+	transform := map[string]interface{}{
+		"x": "y",
+	}
+	transformErr := Transform(&message, transform)
+	assert.Nil(transformErr)
+	assert.Equal(message["a"], "b")
+	assert.Equal(message["x"], "y")
+}
